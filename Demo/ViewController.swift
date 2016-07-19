8b570c47f0edd3a194aaebcbd3f9db28ce8d99a7
jackliaoall <jackliaoall@gmail.com>
2016-07-19 23:06:42 +0800

    Initial Commit

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITextFieldDelegate, UITextViewDelegate {
+    
+    
+    @IBOutlet var textViewCount: UILabel!
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if range.location >= 10 {
+            return false
+        }
+        
+        textViewCount.text = "TextField Word Count: \(range.location + 1)"
+        
+        return true
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        if range.location >= 10 {
+            return false
+        }
+        
+        textViewCount.text = "TextView Word Count: \(range.location + 1)"
+        
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
